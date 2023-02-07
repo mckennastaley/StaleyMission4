72bac2f6531bab6118e532d555c28052a07c5ad2
McKenna Staley <mnnorton@byu.edu>
2023-02-06 22:29:37 -0700

    validation and range added

--- StaleyMission4/Models/calcmodel.cs
+++ StaleyMission4/Models/calcmodel.cs
@@ -9,12 +9,22 @@ namespace StaleyMission4.Models
     public class calcmodel
     {
         [Required]
-
+        [Range (0,100)]
         public decimal Assignment { get; set; }
+        [Required]
+        [Range(0, 100)]
         public decimal GroupProject { get; set; }
+        [Required]
+        [Range(0, 100)]
         public decimal Quiz { get; set; }
+        [Required]
+        [Range(0, 100)]
         public decimal Midterm { get; set; }
+        [Required]
+        [Range(0, 100)]
         public decimal Final { get; set; }
+        [Required]
+        [Range(0, 100)]
         public decimal Intex { get; set; }
 
     }
